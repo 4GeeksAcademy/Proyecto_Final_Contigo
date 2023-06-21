8f7a7af5d7798e8d2a757065be07daf49fd9da72
Marco_acm <marko12cm@gmail.com>
2023-06-21 20:09:45 +0000

    Cambios a navbar y footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import logoCorazonUrl from "../../img/logo_contigo_corazon.png";
+import "../../styles/footer.css";
 
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
-);
+export const Footer = () => {
+  return (
+    <div className="container-fluid footer">
+      <img src={logoCorazonUrl} />
+    </div>
+  );
+};
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logoContigoUrl from "../../img/Logo_Contigo1.png";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <div className="navbar navbar-light">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar logo">
+            <img src={logoContigoUrl} />
+          </span>
+        </Link>
+
+        <ul className="nav justify-content-end">
+          <li className="nav-item">
+            <Link to="/donar">
+              <button className="btn">Donaciones</button>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/informacion">
+              <button className="btn" href="#">
+                Sobre Nosotros
+              </button>
+            </Link>
+          </li>
+          <li className="nav-item ">
+            <Link to="/contacto">
+              <button className="btn" href="#">
+                Contactanos
+              </button>
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/login">
+              <button className="btn login" href="#">
+                <i className="fa-solid fa-user"></i>
+              </button>
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/demo">
+              <button className="btn lenguaje">
+                <i className="fa-solid fa-earth-europe"></i>
+              </button>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
